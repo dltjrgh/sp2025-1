c900cd1ea63554785d9942ea59726449cb5f3efd
root <root@sp-vm.myguest.virtualbox.org>
2025-10-17 12:12:25 +0000

    remove header

--- Sources/run_one.sh
+++ Sources/run_one.sh
@@ -27,9 +27,9 @@ rm -f "${TARGET}" "${APP_OUT}" "${DISK_OUT}"
 echo "${DIRTY}" > /proc/sys/vm/dirty_ratio
 
 # 3) start both bpftrace scripts in background
-BPFTRACE_BTF=/sys/kernel/btf/vmlinux bpftrace app_latency.bt > "${APP_OUT}" &
+bpftrace app_latency.bt > "${APP_OUT}" &
 PID_APP=$!
-BPFTRACE_BTF=/sys/kernel/btf/vmlinux bpftrace disk_io_latency.bt > "${DISK_OUT}" &
+bpftrace disk_io_latency.bt > "${DISK_OUT}" &
 PID_DISK=$!
 
 sleep 1
